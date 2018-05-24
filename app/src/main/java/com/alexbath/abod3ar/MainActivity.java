1850e2f48573a28cb78ffbe817dfe8574d7eaeb4
alex <alexandrosrotsides@gmail.com>
2018-05-24 09:41:37 +0100

    opecv working - some UI changes

--- app/src/main/java/com/alexbath/abod3ar/MainActivity.java
+++ app/src/main/java/com/alexbath/abod3ar/MainActivity.java
@@ -1,6 +1,7 @@
 package com.alexbath.abod3ar;
 
 import android.os.Bundle;
+import android.view.SurfaceView;
 import android.widget.TextView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,9 +12,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    CameraBridgeViewBase cameraBridgeViewBase;
+    Mat mat1,mat2,mat3;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -24,24 +32,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.openCVCameraView);
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
 
         if(OpenCVLoader.initDebug()){
-            Toast.makeText(getApplicationContext(),"aman",Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(),"OpenCV success",Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(getApplicationContext(),"OpenCV failed",Toast.LENGTH_LONG).show();
         }
     }
 
@@ -72,4 +75,57 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mat1 = new Mat(width,height, CvType.CV_8UC4);
+        mat2 = new Mat(width,height, CvType.CV_8UC4);
+        mat3 = new Mat(width,height, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mat1.release();
+        mat2.release();
+        mat3.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+
+        mat1 = inputFrame.rgba();
+
+        return mat1;
+    }
+
+    @Override
+    protected void onPause() {
+
+        if(cameraBridgeViewBase != null){
+            cameraBridgeViewBase.disableView();
+        }
+        super.onPause();
+
+    }
+
+    @Override
+    protected void onResume() {
+
+        if(!OpenCVLoader.initDebug()){
+            Toast.makeText(getApplicationContext(),"OpenCV problem!",Toast.LENGTH_LONG).show();
+        }else{
+
+        }
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        if(cameraBridgeViewBase != null){
+            cameraBridgeViewBase.disableView();
+        }
+        super.onDestroy();
+    }
 }
