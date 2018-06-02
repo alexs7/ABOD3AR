f6235a53e7020c97e66743abcde0aa0915458238
alex <alexandrosrotsides@gmail.com>
2018-06-02 15:33:25 +0100

    added abod3 code for reading plan - only 1 level

---
+++ app/src/main/java/com/alexbath/abod3ar/PlanLoader.java
@@ -0,0 +1,13 @@
+package com.alexbath.abod3ar;
+
+import com.recklesscoding.abode.core.plan.reader.inst.InstPlanReader;
+import java.io.File;
+
+class PlanLoader {
+    public static void loadPlan(String planPath) {
+
+        File planFile = new File(planPath);
+        InstPlanReader planReader = new InstPlanReader();
+
+    }
+}
